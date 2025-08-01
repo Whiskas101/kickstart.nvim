95fa2202acd84c5a83e3af44358a8cda9f978a71
Whiskas101 <getintouchwithvic@gmail.com>
2025-08-01 21:31:22 +0530

    silly insertion fix

--- init.lua
+++ init.lua
@@ -45,7 +45,7 @@ Kickstart Guide:
 
   TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
 
-    If you don't =know what this means, type the following:
+    If you don't know what this means, type the following:
       - <escape key>
       - :
       - Tutor
